a68b5f9b56175b9036fedba68067513b6f520e9b
hujjarh <hujjartullarh@gmail.com>
2024-10-30 08:49:59 +0100

    modify keyframe

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -96,8 +96,8 @@ const Wrapper = styled.div`
         0% {
             transform: translateX(0);
         }
-        50% {
-            transform: translateX(-50%);
+        100% {
+            transform: translateX(-100%);
         }
     }
 
